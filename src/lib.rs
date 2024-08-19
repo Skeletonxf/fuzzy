83bed14be1a6b74d542d7c69aa9582227d6ce869
Skeletonxf <skeletonxf@gmail.com>
2024-08-19 20:27:00 +0100

    Add more documentation and test coverage

--- src/lib.rs
+++ src/lib.rs
@@ -6,10 +6,19 @@
 ///
 /// See also:
 /// - [Levenshtein distance](https://en.wikipedia.org/wiki/Levenshtein_distance)
+// TODO: Need to tweak iteration and length calculations to use charcters not byte lengths
 pub fn levenshtein_distance(source: &str, target: &str) -> usize {
-    // FIXME: Return immediately if source or target are empty to avoid indexing out of bounds
+    // If either input is empty then the shortest transformation is all deletions or insertions
+    // from/to an empty string, which will be equal to the number of characters in the other input
+    // This check also guards against any index out of bounds issues in the main implementation
+    if source.is_empty() {
+        return target.len();
+    }
+    if target.is_empty() {
+        return source.len();
+    }
 
-    // We'll have a matrix A of source length + 1 rows and target length + 1 columns
+    // We'll have a matrix A of `source` length + 1 rows and `target` length + 1 columns
     // This stores the edit distances for prefixes of source and target from the empty string
     // through to the entire inputs.
     // A[0, 0] is therefore "" to "" which is 0, and A[source length + 1, target length + 1] is
@@ -17,35 +26,50 @@ pub fn levenshtein_distance(source: &str, target: &str) -> usize {
     // We only need to store two rows at a time so we never construct this matrix.
 
     let mut edit_distances = vec![0; target.len() + 1];
-    // First row of edit distances are converting an empty string source to prefixes of length 0
-    // to the entire target, "" to "" is 0 edits, "" to one character is one insertion, and so on.
+    // First row of edit distances are converting an empty string `source` to prefixes of length 0
+    // to the entire `target`, "" to "" is 0 edits, "" to one character is one insertion, and
+    // so on through to the entire target string.
     for (i, x) in edit_distances.iter_mut().enumerate() {
         *x = i;
     }
 
     for i in 0..source.len() {
         // Step through each subsequent row of the matrix of edit distances, each time looking at
-        // a prefix of source one character longer
+        // a prefix of `source` one character longer
         let mut new_edit_distances = vec![0; target.len() + 1];
-        // We're on the i+1 prefix of characters in source, so converting this to an empty string
+        // We're on the i+1 prefix of characters in `source`, so converting this to an empty string
         // (the 0 character prefix of target) is purely deletions equal to the length of the
         // source.
         new_edit_distances[0] = i + 1;
 
         for j in 0..target.len() {
-            // Step through columns for the prefixes of target on this prefix of source row
+            // Step through columns for the prefixes of `target` on this prefix of `source` row.
+            // For a source of "kitten" and a target of "sitting", if we were up to i = 1 and
+            // j = 2 then this would look like a source of "ki" we already have the distance for
+            // converting to "si" and we now need to work out the distance to convert to "sit".
+            // We're now calculating the edit distance for A[i + 1, j + 1]
 
-            // TODO: Comments for reason on each of these
+            // At A[i, j + 1] we have the cost to reach the same `target` prefix with a source
+            // that was one character shorter, so we can delete the extraneous character and the
+            // distance could be 1 greater
             let deletion = edit_distances[j + 1] + 1;
+            // At A[i + 1, j] we have the cost to reach a shorter `target` prefix with the same
+            // source, so we can insert the extra character and the distance could be 1 greater
             let insertion = new_edit_distances[j] + 1;
             // FIXME: Need to use character length not byte length and index by chars here
             // this won't handle non ASCII inputs properly right now
             let substitution = if source[i..(i+1)] == target[j..(j+1)] {
+                // If the `source` character at i and the `target` character at j match, we
+                // don't need to transform anything
                 edit_distances[j]
             } else {
+                // Otherwise we can transform the character to match the target, and the distance
+                // could be 1 greater
                 edit_distances[j] + 1
             };
 
+            // We always pick the cheapest option from the 3 we could do, which populates
+            // A[i + 1, j + 1]
             new_edit_distances[j + 1] = std::cmp::min(
                 deletion, std::cmp::min(insertion, substitution)
             );
@@ -53,6 +77,8 @@ pub fn levenshtein_distance(source: &str, target: &str) -> usize {
 
         edit_distances = new_edit_distances;
     }
+    // The distance from `target` to `source` will be the final entry in the array as this
+    // is the full strings of both with no characters ignored.
     edit_distances[target.len()]
 }
 
@@ -75,10 +101,50 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
+    fn transforming_input() {
         let kitten = "kitten";
         let sitting = "sitting";
         let result = levenshtein_distance(&kitten, &sitting);
         assert_eq!(result, 3);
+    }
+
+    #[test]
+    fn adding_a_character() {
+        let result = levenshtein_distance(&"rust", &"rusty");
+        assert_eq!(result, 1);
+    }
+
+    #[test]
+    fn removing_characters() {
+        let result = levenshtein_distance(&"ferrisground", &"run");
+        // run is present in the source input, so shortest transformation is removing the other
+        // characters
+        assert_eq!(result, 9);
+    }
+
+    #[test]
+    fn empty_source() {
+        let result = levenshtein_distance(&"", &"rust");
+        assert_eq!(result, 4);
+    }
+
+    #[test]
+    fn empty_target() {
+        let result = levenshtein_distance(&"bug", &"");
+        assert_eq!(result, 3);
+    }
+
+    #[test]
+    fn multiple_transformations() {
+        let result = levenshtein_distance(&"Edit distance", &"Eddy");
+        // Edd already present in input, so can delete all the other characters and insert y,
+        // so 3 edits fewer than the source input
+        assert_eq!(result, 10);
+    }
+
+    #[test]
+    fn unrelated() {
+        let result = levenshtein_distance(&"unrelated", &"SCREAMING");
+        assert_eq!(result, 9);
     }
 }
